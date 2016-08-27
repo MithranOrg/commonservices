557ab972d119da4c8cd615764ff69b84c2108b29
arunkumarv <arunkumarv@arunkumarv-ThinkPad-Edge-E430>
2016-08-27 14:15:18 +0530

    Registration

---
+++ genericServices/src/main/java/com/nura/db/entity/UserDetails.java
@@ -0,0 +1,210 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nura.db.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Arun
+ */
+@Entity
+@Table(name = "user_dtls")
+public class UserDetails implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "seq")
+    private int seq;
+    @Column(name = "user_id")
+    private String userid;
+    @Column(name = "pwd")
+    private String password;
+    @Column(name = "role_type")
+    private String roleType;
+    @Column(name = "mailiid")
+    private String mailid;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "phone_no")
+    private String phoneNo;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "gender")
+    private String gender;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "dob")
+    private java.util.Date dob;
+    @Column(name = "fullName")
+    private String fullName;
+    
+    public UserDetails(){
+        
+    }
+    
+    /**
+     * @return the seq
+     */
+    public int getSeq() {
+        return seq;
+    }
+
+    /**
+     * @param seq the seq to set
+     */
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+    /**
+     * @return the userid
+     */
+    public String getUserid() {
+        return userid;
+    }
+
+    /**
+     * @param userid the userid to set
+     */
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the roleType
+     */
+    public String getRoleType() {
+        return roleType;
+    }
+
+    /**
+     * @param roleType the roleType to set
+     */
+    public void setRoleType(String roleType) {
+        this.roleType = roleType;
+    }
+
+    /**
+     * @return the mailid
+     */
+    public String getMailid() {
+        return mailid;
+    }
+
+    /**
+     * @param mailid the mailid to set
+     */
+    public void setMailid(String mailid) {
+        this.mailid = mailid;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the phoneNo
+     */
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    /**
+     * @param phoneNo the phoneNo to set
+     */
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the fullName
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * @param fullName the fullName to set
+     */
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    /**
+     * @return the dob
+     */
+    public java.util.Date getDob() {
+        return dob;
+    }
+
+    /**
+     * @param dob the dob to set
+     */
+    public void setDob(java.util.Date dob) {
+        this.dob = dob;
+    }
+    
+}
